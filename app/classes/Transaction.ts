6a9847c54043b9dd26076e73613321ba6b61338a
Gabrielox20 <113552881+Gabrielox20@users.noreply.github.com>
2023-10-10 19:15:33 -0300

    Add files via upload

--- app/classes/Transaction.ts
+++ app/classes/Transaction.ts
@@ -1,7 +1,28 @@
+import { SHA256, enc, HmacSHA256 } from 'crypto-js';
+
 export class Transaction {
-    constructor(
-        public sender: string,
-        public recipient: string,
-        public amount: number
-    ){}
+    sender: string;
+    recipient: string;
+    amount: number;
+    signature: string;
+    valido: boolean;
+
+    constructor(sender: string, recipient: string, amount: number, privateKey?: string, publicKey?: string) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
+        this.signature = this.signTransaction(privateKey); // Firmar la transacción al crearla
+        this.valido = this.verifySignature(publicKey); // Firmar la transacción al crearla
+    }
+
+    signTransaction(privateKey?: string): string {
+        const dataToSign = `${this.sender}${this.recipient}${this.amount}`;
+        const signature = SHA256(dataToSign, privateKey).toString(enc.Hex);
+        return signature;
+    }
+    verifySignature(publicKey?: string): boolean {
+        const dataToSign = `${this.sender}${this.recipient}${this.amount}`;
+        const computedSignature = SHA256(dataToSign, publicKey).toString(enc.Hex);
+        return computedSignature === this.signature;
+    }
 }
