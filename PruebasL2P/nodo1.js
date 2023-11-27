56b9c7c988f5a33e5d53a20a39f59e9cd946f56d
Gabrielox20 <113552881+Gabrielox20@users.noreply.github.com>
2023-11-27 17:27:20 -0300

    Integracion
    Adaptacion Menu

--- PruebasL2P/nodo1.js
+++ PruebasL2P/nodo1.js
@@ -63,8 +63,102 @@ async function sendMessageToAll(message) {
 const node = new Node(port);
 node.start();
 
+import { Transaction } from "../app/classes/Transaction.js";
+import { Block } from "../app/classes/Block.js";
+import { Account } from "../app/classes/Account.js";
+import { DBBlocks, DBAccounts } from "../app/classes/DBManager.js";
+import * as input from 'readline-sync';
+
+const generateBlock = (index, previousHash, transactions) => {
+    return new Block(index, Date.now(), transactions, previousHash);
+};
+
+const generateUniqueAccounts = (numberOfAccounts) => {
+    const accounts = [];
+
+    for (let i = 0; i < numberOfAccounts; i++) {
+        const account = new Account("");
+        accounts.push(account);
+    }
+
+    return accounts;
+};
+
+const registerUser = async (db_accounts, name) => {
+    return await db_accounts.registerAccount(name);
+};
+const db_blocks = new DBBlocks(`./db/db_blocks_${port}.db`);
+const db_accounts = new DBAccounts(`./db/db_accounts_${port}.db`);
+
+const n = 5;
+let transactions = [];
+
+    
+
 // Para enviar mensajes desde la línea de comandos
 process.stdin.on('data', async (data) => {
-  const message = data.toString().trim();
-  await sendMessageToAll(message);
+  while (true) {
+    console.log("Menu:");
+    console.log("1. Register account");
+    console.log("2. Make transaction");
+    console.log("3. Get account of address");
+    console.log("4. View blocks");
+    console.log("5. Exit");
+
+    const choice = input.question('Enter Number Choice: ');
+
+    switch (choice) {
+        case '1':
+            const name = input.question('Enter name for account: ');
+            const message = name.toString().trim();
+            //await sendMessageToAll(message);
+            const aux = await registerUser(db_accounts, message);
+            const finalmessage = message + "-" + aux.toString().trim();
+            await sendMessageToAll(finalmessage);
+            //await sendMessageToAll(await registerUser(db_accounts, message));
+            break;
+        case '2':
+            const sender_address = input.question('Input address sender: ');
+            const recipient_address = input.question('Input address recipient: ');
+            const amount = parseInt(input.question('Input amount: '));
+            const content_transaction = sender_address + "-" + recipient_address + "-" + amount;
+            await sendMessageToAll(content_transaction);
+
+            const sender = await db_accounts.getAccountByAddress(sender_address);
+            const recipient = await db_accounts.getAccountByAddress(recipient_address);
+            
+
+            if(sender != null && recipient != null) {
+                const transaction_generated = new Transaction(sender.name, recipient.name, amount, 1001, sender.privatekey, sender.publickey);
+                
+                const last_block = await db_blocks.getLastBlock();
+                // SI EL BLOQUE TIENE N TRANSACCIONES
+                if(last_block) {
+                    if(last_block?.transactions.length == n) {
+                        console.log(await db_blocks.saveBlock(await generateBlock(last_block.index+1, last_block.hash, [transaction_generated])));
+                    }
+                    else {
+                        last_block.transactions.push(transaction_generated);
+                        console.log(db_blocks.saveBlock(last_block));
+                    }
+                }
+            }
+            
+            break;
+        case '3':
+            const address = input.question('Enter address for get account: ');
+            console.log(await db_accounts.getAccountByAddress(address));
+            break;
+        case '4':
+            await db_blocks.printBlocks();
+            break;
+        case '5':
+            console.log("Exiting...");
+            return;
+        default:
+            console.log("Not Valid Choice");
+    }
+}
+
+  
 });
